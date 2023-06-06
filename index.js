c4ad4bfb5ffe862e3ac32f9568eb26d0dcc26086
Dhanwan <ntflabsdev@gmail.com>
2023-06-06 19:10:25 +0530

    Chcecking CI 3

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
-const http = require('http');
+const express = require('express')
+const app = express()
+const axios = require('axios');
 const port = process.env.PORT || 3000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  const msg = 'Azure DevOps V3\n'
-  res.end(msg);
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/health', async (req, res) => {
+  const url = "http://localhost:4000/";
+  try {
+    const response = await axios.get(url);
+    const result = response.data;
+    res.json({
+      status: "200",
+      data: result
+    });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 });
 
-server.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}/`);
-});
+ 
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
