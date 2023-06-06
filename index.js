fca6f26a29fa2f27987ca5898fe30b5f3d06a012
Dhanwan <ntflabsdev@gmail.com>
2023-06-06 18:34:35 +0530

    Chcecking CI

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Azure DevOps\n'
+  const msg = 'Azure DevOps V2\n'
   res.end(msg);
 });
 
